8a7bc15539729b884ff4556f5d56d4f536be8a08
Alain <albulaya@gmail.com>
2022-02-19 16:02:00 +0100

    Mis en place de navbar

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tableau de bord') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200 text-center" >
+                    <b>Maintenant que vous êtes connectés, vous devez suivre la procedure suivante:</b>
+                
+                    <img src="{{asset('/images/liste.jpg')}}" ; width="30px" alt="liste">Générer un numero de suivi qui identifiera un enlèvement,
+                    <img src="{{asset('/images/liste.jpg')}}" ; width="30px" alt="liste">Complété les données avant d'imprimer un bordereau,
+                    <img src="{{asset('/images/liste.jpg')}}" ; width="30px" alt="liste">Générer un code Tracking pour chaque materiel valorisé et enfin
+                    <img src="{{asset('/images/liste.jpg')}}" ; width="30px" alt="liste">Compléter une fiche pour chaque matériel valorisé.
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/suivi', function () {
+    return view('suivi');
+})->middleware(['auth'])->name('suivi');
+
+Route::get('bordereau', function () {
+    return view('bordereau');
+})->middleware(['auth'])->name('bordereau');
+
+Route::get('/codeTracking', function () {
+    return view('codeTracking');
+})->middleware(['auth'])->name('codeTracking');
+
+Route::get('/fiche', function () {
+    return view('fiche');
+})->middleware(['auth'])->name('fiche');
+
+require __DIR__.'/auth.php';
